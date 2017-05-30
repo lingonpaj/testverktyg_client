c6a4391da8dc0498ac3e802afc3a993f1f0829b4
Mikael Rusin <mikaelgodlike@hotmail.com>
2017-05-30 11:58:10 +0200

    Added Teacherview
    The Teacherview features a choice box of completed tests for a respective user so that we can select them and have the tableview be updated based on the person chosen in the dropdown menu

--- src/testverktygclient/LoginController.java
+++ src/testverktygclient/LoginController.java
@@ -5,9 +5,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import testverktygclient.models.User;
 import testverktygclient.serverconnection.ServerConnection;
 
@@ -19,6 +25,12 @@ public class LoginController implements Initializable {
     private TextField password;
     @FXML
     private Text errorLogin;
+    
+    Stage stage;
+    Parent root;
+    
+    @FXML
+    private Button loginButton;
     
     private ServerConnection serverConnection;
     
@@ -33,9 +45,19 @@ public class LoginController implements Initializable {
         }
     }
     
-    private void loadNextPage(User userToLogin) {
+    private void loadNextPage(User userToLogin) throws IOException {
         //Temporary, will be replaced by loading of page depending of type of user
         errorLogin.setText(userToLogin.getClass().getSimpleName() + " authorized");
+        
+        
+        Stage ourStage = (Stage) loginButton.getScene().getWindow();
+        
+        root = FXMLLoader.load(getClass().getResource("" + userToLogin.getClass().getSimpleName() + "View.fxml"));
+        ourStage.setScene(new Scene(root));
+        ourStage.setTitle("The " + userToLogin.getClass().getSimpleName() + " view!");
+        
+       
+        
     }
     
     @Override
